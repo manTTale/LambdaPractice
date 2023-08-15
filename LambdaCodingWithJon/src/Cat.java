23633c80986a255946d70c3fffb145028ba1d8f9
manTTale <mantale.roberto1@yahoo.com>
2023-08-15 21:32:51 +0300

    how to use print method wihtouth lambdas

---
+++ LambdaCodingWithJon/src/Cat.java
@@ -0,0 +1,10 @@
+public class Cat implements Printable{
+
+    String name;
+    int age;
+
+    @Override
+    public void print() {
+        System.out.println("Meow");
+    }
+}
--- LambdaCodingWithJon/src/Main.java
+++ LambdaCodingWithJon/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
+
+//TUTORIAL FOLLOWED -> https://www.youtube.com/watch?v=tj5sLSFjVj4
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cat cat = new Cat();
+        printThing(cat);
+        //Without lambdas -> What was done step by step
+        //1. The Cat class implemented the Printable interface and also inherited the method
+        //2. Define the printThing method with has as a parameter an object that implements the printable interface
+        //printThing(cat)
+    }
+
+    public static void printThing(Printable thing){
+        thing.print();
     }
 }
